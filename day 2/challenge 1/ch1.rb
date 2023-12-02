6c374852b6d7ab9b03516dbb47013675752a688e
Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2023-12-02 14:15:38 -0600

    Refine challenge code to original idea with using match

--- day 2/challenge 1/ch1.rb
+++ day 2/challenge 1/ch1.rb
@@ -1,25 +1,12 @@
 # only 12 reds, 13 greens, 14 blues
-
-bag = {'red' => 12, 'green'=> 13, 'blue' => 14}
-ans = 0
-
+sum = 0
 fileData = File.read('input.txt')
 
 fileData.each_line do |line|
-  ok = true
   id_, line = line.split(':')
-  line.split(';').each do |r|
-    r.split(',').each do |b|
-      n, c = b.split
 
-      if n.to_i > bag[c]
-        ok = false
-      end
-    end
-  end
-  if ok
-    ans += id_.split()[-1].to_i
-    puts id_
+  if !(line.match?(/([2-9][0-9])|(1[5-9])|([1-9][4-9]\sgreen)|([1][3-9]\sred)/))
+    sum += id_.split()[-1].to_i
   end
 end
-puts ans
+puts sum
