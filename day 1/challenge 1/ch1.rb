e417890ea2aefdfd4ef7f755fb9f40ac847175ae
Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2023-12-01 19:03:46 -0600

    Finish Challenge One Day One

---
+++ day 1/challenge 1/ch1.rb
@@ -0,0 +1,9 @@
+num_data = []
+
+file_data = File.read('input.txt')
+
+file_data.each_line { |line| num_data << line.delete('/[a-zA-Z]/') }
+puts num_data.class
+puts "end of"
+filter_data = num_data.map { |num| (num.chars.values_at(0, -2).join).to_i }
+puts filter_data.sum
