254d4cd6575b3f7544d2da320e912fd61fbb6034
Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2023-12-02 13:27:04 -0600

    Finish Day 2 Challenge 2

---
+++ day 2/challenge 2/ch2.rb
@@ -0,0 +1,29 @@
+# find max of each color to make game possible
+
+bag = {'red' => 0, 'green'=> 0, 'blue' => 0}
+power = []
+ans = 0
+
+fileData = File.read('input.txt')
+
+fileData.each_line do |line|
+
+  id_, line = line.split(':')
+  line.split(';').each do |r|
+    r.split(',').each do |b|
+      n, c = b.split
+
+      if n.to_i > bag[c]
+        bag[c] = n.to_i
+      end
+    end
+  end
+
+  puts id_
+  puts bag
+  power << (bag['green'] * bag['red'] * bag['blue'])
+
+  bag = {'red' => 0, 'green'=> 0, 'blue' => 0}
+end
+puts power.sum
+# puts ans
