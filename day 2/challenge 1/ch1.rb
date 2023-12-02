9a41c720a2e1ecc2d4df2289a8ceb5c16fbf137a
Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2023-12-02 14:17:58 -0600

    Made if statement one line

--- day 2/challenge 1/ch1.rb
+++ day 2/challenge 1/ch1.rb
@@ -5,8 +5,7 @@ fileData = File.read('input.txt')
 fileData.each_line do |line|
   id_, line = line.split(':')
 
-  if !(line.match?(/([2-9][0-9])|(1[5-9])|([1-9][4-9]\sgreen)|([1][3-9]\sred)/))
-    sum += id_.split()[-1].to_i
-  end
+  sum += id_.split()[-1].to_i if !(line.match?(/([2-9][0-9])|(1[5-9])|([1-9][4-9]\sgreen)|([1][3-9]\sred)/))
+
 end
 puts sum
