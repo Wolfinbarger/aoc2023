0376a1dafc37dabb40135ad5526b820b8d5b33d6
Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2023-12-05 22:04:11 -0600

    finish part 1 of Day 5

---
+++ day 4/challenge 1/ch1.rb
@@ -0,0 +1,24 @@
+
+ans = 0
+
+fileDataTest = File.open('input.txt')
+
+fileDataTest.each_line do |line|
+  points = 1
+  n = 1
+
+  id_, card = line.chomp!.split(':')
+  winningNums, myNums = card.split('|')
+
+  matches = (winningNums.split() & myNums.split()).length
+
+  (matches - 1).times do
+    points += 2 ** (n - 1)
+    n += 1
+  end
+
+  ans += points if matches != 0
+
+end
+
+p ans
---
+++ day 5/ch1.rb
@@ -0,0 +1,55 @@
+fileData = File.read('input.txt').split(/\n\n/)
+
+seeds = fileData.shift().split(": ")[1].split(" ")
+
+def createRange(line)
+  items = line.split
+
+  return {
+    dest: items[0].to_i,
+    src: items[1].to_i,
+    range: items[2].to_i,
+  }
+end
+
+def parseMap(fileData)
+  data = fileData.split(/\n/)
+  from, _, to = data.shift().split(':')[0].split()[0].split('-')
+
+  return {
+      from: from,
+      to: to,
+      map: data.map { |x| createRange(x) }
+    }
+
+end
+
+def getLocation(value, name, map)
+
+  return value if map.find { |e| e[name]}.nil?
+
+  item = map.find { |e| e[name]}
+
+  range = item[name][:map].find { |x| x[:src] <= value && value <= x[:src] + x[:range] }
+
+
+  if (!range.nil?)
+    newValue = range[:dest] + (value - range[:src])
+    return getLocation(newValue, item[name][:to], map)
+  end
+
+  return getLocation(value, item[name][:to], map)
+end
+
+maps = fileData.map { |x| parseMap(x) }
+
+parsedMaps = maps.map do |hash|
+  { hash[:from] => hash }
+end
+
+minLocation = nil
+seeds.each do |seed|
+  location = getLocation(seed.to_i, 'seed', parsedMaps)
+  ( minLocation.nil? || location < minLocation  ) ? minLocation = location : minLocation
+end
+p minLocation
