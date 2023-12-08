33d9837c244f48ba7225f938bda89a13501db89e
Wolfinbarger <xzaiver.wolfinbarger@gmail.com>
2023-12-07 22:52:47 -0600

    rename method binarySearch to search as binary search was not implemented

--- day 6/ch1.rb
+++ day 6/ch1.rb
@@ -1,6 +1,6 @@
 times, distances = File.read('input.txt').split(/\n/).map { |ele| ele.split(':')[1].split.map(&:to_i) }
 
-def binarySearch(time, mid, distance)
+def search(time, mid, distance)
   mid.times do |i|
     if ((time - i) * i ) > distance
       return time.odd? ? (mid - i) * 2 : (((mid  - i) * 2) - 1)
@@ -14,7 +14,7 @@ def getCombos(times, distances)
     length = time + 1
     midPoint = (length % 2).zero? ? length / 2 : length / 2 + 1
     mid = ( midPoint % 2).zero? ? midPoint / 2 : midPoint / 2 + 1
-    combos << binarySearch(time, midPoint, distances[index])
+    combos << search(time, midPoint, distances[index])
   end
   combos
 end
--- day 6/ch2.rb
+++ day 6/ch2.rb
@@ -1,7 +1,7 @@
 time, distance = File.read('input.txt').split(/\n/).map { |ele| ele.split(':')[1].delete(' ').to_i }
 
 
-def binarySearch(time, mid, distance)
+def search(time, mid, distance)
   mid.times do |i|
     if ((time - i) * i ) > distance
       return time.odd? ? (mid - i) * 2 : (((mid  - i) * 2) - 1)
@@ -14,7 +14,7 @@ def getCombos(time, distance)
   length = time + 1
   midPoint = (length % 2).zero? ? length / 2 : length / 2 + 1
   mid = ( midPoint % 2).zero? ? midPoint / 2 : midPoint / 2 + 1
-  binarySearch(time, midPoint, distance)
+  search(time, midPoint, distance)
 end
 
 p getCombos(time, distance)
